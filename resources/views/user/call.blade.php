1a629473584f65b6664c8f1e56f633d0817db909
zhusq <18036161805@163.com>
2022-03-02 11:24:07 +0800

    0

--- resources/views/user/call.blade.php
+++ resources/views/user/call.blade.php
@@ -59,14 +59,23 @@
         </form>
         <div class="button-list">
             <button id="batch_call" class="btn btn-success">开始自动拨号</button>
-            <button class="btn btn-danger">停止自动拨号</button>
-            <button class="btn btn-info">拨号</button>
-            <button id="call" class="btn btn-danger">挂断</button>
-            <button id="hangup" class="btn btn-primary">转为意向客户</button>
+            <button id="batch_hangup" class="btn btn-danger">停止自动拨号</button>
+            <button id="call" class="btn btn-info">拨号</button>
+            <button id="hangup" class="btn btn-danger">挂断</button>
+            <button id="set_intention_user" class="btn btn-primary">转为意向客户</button>
         </div>
     </div>
 </div>
 <script>
+    //初始化设备
+    var callout_cb;
+    init();
+
+    function init() {
+        getWebsocket();
+    }
+
+    //点击右侧列表，把信息传到表单
     $('.user-info').click(function () {
         var id = $(this).data('id');
         var user_id = $(this).data('user_id');
@@ -79,86 +88,119 @@
         $('.form-mobile').val(mobile);
     });
 
-    $('#batch_call').click(function () {
-        $(".user-info-list ul li").each(function (key, item) {
-            var mobile = $(item).data('mobile');
-console.log(mobile);
-            return false;
-        })
+    //单独拨号
+    $('#call').click(function () {
+        var mobile = $('.form-mobile').val();
+        if (!mobile) {
+            alert('请先选择需要拨打的用户号码');
+        } else {
+            Call(mobile);
+        }
     });
 
-    function callMobile(mobile) {
+    //单独挂机
+    $('#hangup').click(function () {
+        hangup()
+    });
 
+
+    //
+    // $('#batch_call').click(function () {
+    //     $(".user-info-list ul li").each(function (key, item) {
+    //         var mobile = $(item).data('mobile');
+    //         // Call(mobile);
+    //         //30秒后自动挂机
+    //         setTimeout("hangup()",30000 );
+    //     })
+    // });
+
+    //主动切换下一张卡拨号
+    $('#next').click(function () {
+        ws.send(JSON.stringify({action: 'SimNext', cb: new Date().getTime()}));
+        ws.onmessage = function (event) {
+            console.log("message", event.data);
+        };
+        ws.onerror = function () {
+            console.log("error");
+        }
+    });
+
+    function Call(number) {
+        if (!ws) {
+            alert("控件未初始化");
+            return false;
+        }
+        callout_cb = 'CallOut_cb_' + new Date().getTime();
+        var action = {
+            action: 'CallOut',
+            number: number,
+            cb: callout_cb
+        };
+        ws.send(JSON.stringify(action));
+        //收到服务端消息
+        ws.onmessage = function (event) {
+            console.log("message", event.data);
+            var data = JSON.parse(event.data);
+            var message = data.message;
+            var param = data.param;
+            var status = param.status;
+            if (status == 'TalkingStart') {
+                console.log('语音开始');
+            } else if (status == 'TalkingEnd') {
+                console.log("语音结束");
+                uploadFile();
+            } else if (status == 'CallEnd') {
+                console.log("通话结束:我个人理解为未接通或者挂机");
+            }
+        };
+        //发生错误
+        ws.onerror = function () {
+            console.log("error");
+        }
     }
 
+    function uploadFile() {
+        ws.send(
+            JSON.stringify({
+                action: 'Settings',
+                settings: {
+                    upload: {
+                        api: 'https://www.test.com/upload',
+                        flag: 'token-1234-123',
+                        file: '1',
+                        qiniu: {
+                            AccessSecret: 'oQ3ZTYyikPQ_jzQ5ryohIUly4v6hZTTWP36VcaqM_self',
+                            AccessKey: 'SQxedWGQY-XiP1XrEODpdgmi4kiCM9yvQJ0-mHBI_sefl',
+                            Zone: 'Zone_z1',
+                            Bucket: 'pdcallertest'
+                        }
+                    }
+                },
+                cb: new Date().getTime()
+            })
+        );
+    }
 
-    // var callout_cb;
-    // init();
-    //
-    // function init() {
-    //     getWebsocket();
-    // }
-    //
-    // $('#call').click(function () {
-    //     var no = $('.form-mobile').val();
-    //
-    //     Call(no);
-    // });
-    //
-    // $('#hangup').click(function () {
-    //     ws.send(JSON.stringify({action: 'Hangup', cb: new Date().getTime()}));
-    //     ws.onmessage = function (event) {
-    //         console.log("message", event.data);
-    //     };
-    //     ws.onerror = function () {
-    //         console.log("error");
-    //     }
-    // });
-    // $('#next').click(function () {
-    //     ws.send(JSON.stringify({action: 'SimNext', cb: new Date().getTime()}));
-    //     ws.onmessage = function (event) {
-    //         console.log("message", event.data);
-    //     };
-    //     ws.onerror = function () {
-    //         console.log("error");
-    //     }
-    // });
-    //
-    // function Call(number) {
-    //     console.log(number);
-    //     if (!ws) {
-    //         alert("控件未初始化");
-    //         return false;
-    //     }
-    //
-    //     callout_cb = 'CallOut_cb_' + new Date().getTime();
-    //
-    //     var action = {
-    //         action: 'CallOut',
-    //         number: number,
-    //         cb: callout_cb
-    //     };
-    //     ws.send(JSON.stringify(action));
-    //     //收到服务端消息
-    //     ws.onmessage = function (event) {
-    //         console.log("message", event.data);
-    //     };
-    //     //发生错误
-    //     ws.onerror = function () {
-    //         console.log("error");
-    //     }
-    // }
-    //
-    // function getWebsocket() {
-    //     ws = new WebSocket('ws://127.0.0.1:8090/APP_2AD85C71-BEF8-463C-9B4B-B672F603542A_fast');
-    //     ws.onerror = function (event) {
-    //         alert('初始化设备失败：' + event.data);
-    //     };
-    //     ws.onclose = function (event) {
-    //     };
-    //     ws.onopen = function () {
-    //         alert('初始化设备成功');
-    //     }
-    // }
+    function getWebsocket() {
+        ws = new WebSocket('ws://127.0.0.1:8090/APP_2AD85C71-BEF8-463C-9B4B-B672F603542A_fast');
+        ws.onerror = function (event) {
+            alert('初始化设备失败：' + event.data);
+        };
+        ws.onclose = function (event) {
+        };
+        ws.onopen = function () {
+            alert('初始化设备成功');
+        }
+    }
+
+    function hangup() {
+        ws.send(JSON.stringify({action: 'Hangup', cb: new Date().getTime()}));
+        ws.onmessage = function (event) {
+            console.log("message", event.data);
+        };
+        ws.onerror = function () {
+            console.log("error");
+        }
+    }
 </script>
 </body>
